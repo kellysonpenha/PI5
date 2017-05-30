f4e496fd3886eb47eb85467a70d896e772fc57ec
Kellyson Penha <kellyson.penha@gmail.com>
2017-05-30 17:14:42 -0300

    Iniciando controle de acesso

---
+++ seguranca.php
@@ -0,0 +1,17 @@
+<?php
+	require_once "../db.php";
+	session_start();
+	function protegeRestaurante() {
+		if (!isset($_SESSION['idRestaurante'])) {
+			unset($_SESSION['idRestaurante']);
+
+			if (isset($_SESSION['idAluno'])) {
+	    		header("Location: ../index.php");
+			}else{
+	  			$dominio = $_SERVER['HTTP_HOST'];
+ 				$url = "http://" . $dominio. $_SERVER['REQUEST_URI'];
+	    		header("Location: http://localhost:90/PI/PI5/pages/examples/login-restaurante.php?continua=".$url);
+			}
+		}
+	}
+?>
